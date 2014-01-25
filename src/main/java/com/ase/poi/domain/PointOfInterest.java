eed6bdec1eeb66dfd24f6c0ff7c7886921b7462a
TheRealHaui <hausegger.michael@googlemail.com>
2014-01-25 21:21:24 +0100

    JSON support

--- src/main/java/com/ase/poi/domain/PointOfInterest.java
+++ src/main/java/com/ase/poi/domain/PointOfInterest.java
@@ -3,10 +3,12 @@ import org.springframework.roo.addon.javabean.RooJavaBean;
 import org.springframework.roo.addon.jpa.activerecord.RooJpaActiveRecord;
 import org.springframework.roo.addon.tostring.RooToString;
 import javax.validation.constraints.NotNull;
+import org.springframework.roo.addon.json.RooJson;
 
 @RooJavaBean
 @RooToString
 @RooJpaActiveRecord
+@RooJson
 public class PointOfInterest {
 
     /**
